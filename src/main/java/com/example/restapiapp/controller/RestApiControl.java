c35f36250231acae5ce3cc2922931efcbda979b7
Nishan Kumar <nishantkumarkd@gmail.com>
2022-08-29 07:56:54 +0530

    Using Post

--- src/main/java/com/example/restapiapp/controller/RestApiControl.java
+++ src/main/java/com/example/restapiapp/controller/RestApiControl.java
@@ -1,5 +1,6 @@
 package com.example.restapiapp.controller;
 
+import com.example.restapiapp.model.Details;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,5 +14,9 @@ public class RestApiControl {
     @RequestMapping(value = {"/any/{age}"},method = RequestMethod.GET)
     public String getAge(@PathVariable String age){
         return "<h1>You are "+age+" years old.</h1>";
+    }
+    @PostMapping("/post")
+    public String post(@RequestBody Details data){
+        return "Welcome "+data.getFirstName()+" "+data.getLastName()+"!!!";
     }
 }
